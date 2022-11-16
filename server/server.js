088c25fee85a6984f6e103eb06193d628779fd2e
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-11-16 23:28:44 +0400

    koa api created

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("hello world 2");
+const Koa = require("koa");
+const { koaBody } = require("koa-body");
+
+const app = new Koa();
+
+// middleware
+app.use(koaBody());
+
+// Require the routers
+let items = require("./data.js");
+
+// use the routes
+app.use(items.routes());
+
+app.listen(3000);
