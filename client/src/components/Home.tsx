574ba766e7d99c7591fde4a1aa94b532ebc10b85
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-11-17 02:51:34 +0400

    stored data in zustand

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,22 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import useStore from "../store";
 
 function Home() {
-  const [data, setData] = useState<any[]>();
-
-  const fetchData = async () => {
-    const response = await fetch("http://localhost:4000/data");
-    const json = await response.json();
-
-    if (response.ok) {
-      setData(json);
-    }
-  };
-
-  console.log(data);
+  const tableData = useStore((state: any) => state.tableData);
+  const setTableData = useStore((state: any) => state.setTableData);
 
   useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:4000/data");
+      const json = await response.json();
+
+      if (response.ok) {
+        setTableData(json);
+      }
+    };
     fetchData();
-  }, []);
+  }, [setTableData]);
+
+  console.log(tableData, "Axaxa");
   return (
     <div>
       <h1>giorgi</h1>
---
+++ client/src/store.ts
@@ -0,0 +1,12 @@
+import create from "zustand";
+
+const useStore = create((set) => ({
+  tableData: [],
+  setTableData: (tableData: any) =>
+    set((state: any) => ({
+      ...state,
+      tableData,
+    })),
+}));
+
+export default useStore;
